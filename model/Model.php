452f7aef865d17c5266b8830f4c4a33306896b6f
sheldongopi <302624171@student.rocmondriaan.nl>
2020-11-30 17:35:10 +0100

    Login Methorde

---
+++ model/Model.php
@@ -0,0 +1,32 @@
+<?php
+namespace model;
+include_once ('model/User.php');
+
+class Model
+{
+    public function login($username, $password){
+        $this->makeConnection();
+        $selection = $this->database->prepare(
+            'SELECT * FROM `users` WHERE `users`,`username` =:username');
+        $selection->bindParam(":username", $username);
+        $result = $selection->execute();
+        if ($result) {
+            $selection->setFetchMode(\PDO::FETCH_CLASS,
+                \model\User::class);
+            $user = $selection->fetch();
+            if ($user) {
+                $gehashtpassword = strtoupper(hash("sha256", $password));
+                var_dump($gehashtpassword);
+                if ($user->getPassword() == $gehashtpassword) {
+                    $_SESSION['user'] = $user->getUsername();
+                    $_SESSION['role'] = $user->getRole();
+
+                }
+            }
+        }
+    }
+    public function logout(){
+        session_unset();
+        session_destroy();
+    }
+}
